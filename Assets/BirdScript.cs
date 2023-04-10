2c6fb9df0a8024cee6aa3cd8f912c2fd7cb48a45
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-04-09 22:23:11 -0400

    Flappy Assets + Bird Goes Up on Spacebar
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
