ddb21cbb28c9907e54bd342bae8fd107baf02879
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-04-11 23:28:34 -0400

    Game Complete (Tutorial's End)
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,19 +6,27 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength = 10;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        logic = GameObject.FindWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive == true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
