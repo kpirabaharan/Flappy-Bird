06bb8ec7fc2dc1b4207105dc5283f1d0c31b1ac2
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-04-11 23:45:13 -0400

    Score No Longer Goes Up After Game Over
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Assets/PipeMiddleScript.cs
+++ Assets/PipeMiddleScript.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class PipeMiddleScript : MonoBehaviour
 {
-
     public LogicScript logic;
+    public BirdScript bird;
     // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.FindWithTag("Logic").GetComponent<LogicScript>();
+        bird = GameObject.FindWithTag("Bird").GetComponent<BirdScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == 3)
+        if (collision.gameObject.layer == 3 && bird.birdIsAlive == true)
         {
             logic.addScore(1);
         }
