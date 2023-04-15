9a0f6d1324198a8b916e4fbebbc151fb9b1e83b6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-04-14 23:25:27 -0400

    Added High Score with PlayerPrefs
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
+    public int highScore;
     public Text scoreText;
+    public Text highScoreText;
     public GameObject gameOverScreen;
     public AudioSource dingSFX;
 
@@ -19,6 +21,14 @@ public class LogicScript : MonoBehaviour
         dingSFX.Play();
     }
 
+    private void OnEnable()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScoreText.text = PlayerPrefs.GetInt("HighScore").ToString();
+        }
+    }
+
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -27,5 +37,27 @@ public class LogicScript : MonoBehaviour
     public void gameOver()
     {
         gameOverScreen.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        savePrefs();
+    }
+
+    public void savePrefs()
+    {
+        // Set player prefs of HighScore with playerscore
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+            if(highScore < playerScore)
+            {
+                PlayerPrefs.SetInt("HighScore", playerScore);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", playerScore);
+        }
     }
 }
